b5a058d240223fdc3b4a94dbaf03b58ea997ac20
Churchil Owino <chachowino75@gmail.com>
2024-05-24 00:25:17 +0300

    Add sitemap to website

---
+++ src/app/sitemap.ts
@@ -0,0 +1,33 @@
+import { MetadataRoute } from "next";
+
+// Function to escape XML special characters
+const escapeXml = (unsafe: string) => {
+  return unsafe.replace(/[<>&'"]/g, (c) => {
+    switch (c) {
+      case "<":
+        return "&lt;";
+      case ">":
+        return "&gt;";
+      case "&":
+        return "&amp;";
+      case "'":
+        return "&apos;";
+      case '"':
+        return "&quot;";
+      default:
+        return c;
+    }
+  });
+};
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const routes = ["", "about", "reviews"].map((route) => ({
+    url: escapeXml(`https://saruk.co.ke/${route}`),
+    lastModified: escapeXml(new Date().toISOString()),
+  }));
+
+  // Concatenate all arrays into a single array
+  const allUrls = [...routes];
+
+  return allUrls;
+}
