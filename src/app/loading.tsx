463f41c903df60b8945552727cc7df779096c711
Churchil Owino <chachowino75@gmail.com>
2024-05-24 08:15:11 +0300

    Add loading UI

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import LoadingSpinner from "@/components/ui/loadingSpinner";
+
+const Loading: React.FC = () => {
+  return <LoadingSpinner />;
+};
+export default Loading;
